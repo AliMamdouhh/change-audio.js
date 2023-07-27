25914f6ce6da5318f202d43318cd906b73b7b755
AliMamdouhh <103014568+AliMamdouhh@users.noreply.github.com>
2023-07-27 05:13:14 +0300

    change-audio.js

---
+++ change-audio.js
@@ -0,0 +1,44 @@
+
+
+    // استدعاء العناصر اللازمة
+const elements = document.querySelectorAll('a, button, div, span');
+const audioElement = document.getElementById('audio');
+const toggleAudioButton = document.getElementById('toggleAudio');
+
+// تعريف متغير لحالة تشغيل الأصوات
+let audioEnabled = true;
+
+// تحديث قيمة متغير audioEnabled إذا تم تخزين قيمة سابقة
+if (localStorage.getItem('audioEnabled') !== null) {
+  audioEnabled = localStorage.getItem('audioEnabled') === 'true';
+}
+
+// تحديث مظهر زر التحكم في الأصوات
+updateToggleAudioButton();
+
+// إضافة مستمع للضغط على الروابط والأزرار
+elements.forEach((element) => {
+  element.addEventListener('click', (event) => {
+    if (audioEnabled) {
+      audioElement.currentTime = 0;
+      audioElement.play();
+    }
+  });
+});
+
+// إضافة مستمع للضغط على زر التحكم في الأصوات
+toggleAudioButton.addEventListener('click', (event) => {
+  audioEnabled = !audioEnabled;
+  localStorage.setItem('audioEnabled', audioEnabled);
+  updateToggleAudioButton();
+});
+
+// تحديث مظهر زر التحكم في الأصوات بناءً على قيمة متغير audioEnabled
+function updateToggleAudioButton() {
+  if (audioEnabled) {
+    toggleAudioButton.innerHTML = 'إيقاف الصوت';
+  } else {
+    toggleAudioButton.innerHTML = 'تشغيل الصوت';
+  }
+}
+
